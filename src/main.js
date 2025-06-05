247ecb8c138c3384457ef16cc227c649dde8d39e
Sergey Alekseev <s_alekseev@lesta.group>
2025-06-05 16:14:06 +0300

    init commit

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import './css/reset.css';
+import './css/vars.css';
+import './css/style.css';
+import './css/global.css';
+import './css/main.css';
+import './css/container.css';
+import './css/header.css';
+import './css/logo.css';
+import './css/button.css';
+import './css/social.css';
+import './css/footer.css'
+
